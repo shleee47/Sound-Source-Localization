52f5b3ae42d886fe2371f01a3665da41bc5b8844
sanghoon <57610448+shleee47@users.noreply.github.com>
2022-01-14 01:46:06 +0900

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -1,14 +1,13 @@
 conda create -y -n vamos python=3.8
 conda activate vamos
-#source activate vamos
-conda install scipy
 
 ####select according to your conda version####
 ####https://pytorch.org/####
 conda install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia
 #conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
 
-conda install pyaudio
-conda install -c conda-forge librosa
+conda install -y pandas h5py scipy
+conda install -y pysoundfile librosa youtube-dl tqdm -c conda-forge
 
 pip install PyYAML
+pip install tensorboard
